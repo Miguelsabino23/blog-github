49a05673e677f38a7661495356bcab34503a0a00
miguelsabino23 <miguel2012palhada@gmail.com>
2025-01-18 19:05:41 -0300

    feat: configurar ThemeProvider e aplicar estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import "./App.css";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
-  return <h1>Hello Word</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello World</h1>;
+    </ThemeProvider>
+  );
 }
