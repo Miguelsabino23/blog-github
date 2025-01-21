680033fa72f59948411f3d495d8ca639b357a736
miguelsabino23 <miguel2012palhada@gmail.com>
2025-01-21 17:07:10 -0300

    feat: adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { DefaultLayout } from "./layouts/defaultLayout";
+import { Home } from "./pages/home";
+import { PostDetails } from "./pages/postDetails";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Hello World</h1>;
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<DefaultLayout />}>
+            <Route index element={<Home />} />
+            <Route path='/post' element={<PostDetails />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
