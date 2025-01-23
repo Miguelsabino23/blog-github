859ad07866d9e3c48d7618b3859a47b90c4b4e4f
miguelsabino23 <miguel2012palhada@gmail.com>
2025-01-23 18:06:16 -0300

    feat: cria componente homeContainer

---
+++ src/pages/home/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+  background-color: ${(props) => props.theme["base_background"]};
+  height: 100%;
+  padding-bottom: 2rem;
+`;
